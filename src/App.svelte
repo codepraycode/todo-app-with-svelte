056755fe89b07848963571cd28871d6901a275e8
Precious Olusola <preciousolusola16@gmail.com>
2022-11-28 15:34:54 +0100

    Implemented my custom store contract

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,20 @@
     import Alert from './components/Alert.svelte';
 
 
-    let todos = [
-        { id: 1, name: "Create a Svelte starter app", completed: true },
-        { id: 2, name: "Create your first component", completed: true },
-        { id: 3, name: "Complete the rest of the tutorial", completed: false }
-    ];
+    import {todos} from './stores';
+
+
+    // $todos = [
+    //     { id: 1, name: "Create a Svelte starter app", completed: true },
+    //     { id: 2, name: "Create your first component", completed: true },
+    //     { id: 3, name: "Complete the rest of the tutorial", completed: false }
+    // ];
+
+    // $: console.log("todos store -todos:", todos);
+    // $: console.log("todos store content - $todos:", $todos);
 
 </script>
 
 <!-- <Todos {todos}/> -->
 <Alert />
-<Todos {todos}/>
+<Todos bind:todos={$todos}/>
--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,33 @@
 import { writable } from 'svelte/store';
 
+export const localStore = (key, initial) => {                 // receives the key of the local storage and an initial value
+
+    const toString = (value) => JSON.stringify(value, null, 2)  // helper function
+    const toObj = JSON.parse                                    // helper function
+
+    if (localStorage.getItem(key) === null) {                   // item not present in local storage
+        localStorage.setItem(key, toString(initial))              // initialize local storage with initial value
+    }
+
+    const saved = toObj(localStorage.getItem(key))              // convert to object
+
+    const { subscribe, set, update } = writable(saved)          // create the underlying writable store
+
+    return {
+        subscribe,
+        set: (value) => {
+            localStorage.setItem(key, toString(value))              // save also to local storage as a string
+            return set(value)
+        },
+        update
+    }
+}
+
+const initialTodo = [
+    { id: 1, name: 'Visit MDN web docs', completed: true },
+    { id: 2, name: 'Complete the Svelte Tutorial', completed: false },
+]
+
 export const alert = writable("Welcome to the to-do list app!");
 
-
+export const todos = localStore('mdn-svelte-todo', initialTodo);
